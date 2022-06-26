bb8fcdcbf202e4efb3e41853e5ed2077322524ee
Cezar Godoy <60990343+cezargodoy@users.noreply.github.com>
2022-06-25 23:31:55 -0300

    new

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ define("SENHA", "");
 define('CONTROLLER_PADRAO', 'home');
 define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
-define('URL_BASE', 'http://localhost/Projeto_PDWEL');
+define('URL_BASE', 'http://localhost/Projeto_PDWEL/');
 
 
 
